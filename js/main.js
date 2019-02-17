2d7c08992bca8ff323111402c24f89f43eb56056
Artem Komar <wsozdanie@gmail.com>
2019-02-17 21:21:17 +0200

    Add script anchor and menu

--- js/main.js
+++ js/main.js
@@ -16,3 +16,32 @@ var wow = new WOW(
   );
   wow.init();
   // CLOSE  Работа с анимациями при прокрутки страницы. Wow.js
+
+
+// OPEN Адаптивное меню
+  $(function(){
+    var link  = $('.nav__item');
+    var menu  = $('.nav');
+    var close = $('.close-menu');
+    link.on('click', function(event){
+        event.preventDefault();
+        menu.toggleClass('nav');
+    });
+    close.on('click', function(event) {
+        event.preventDefault();
+        menu.toggleClass('nav');
+    });
+});
+// CLOSE Адаптивное меню
+
+
+// OPEN   Якорь. Плавный переход по клику на ссылку.
+$(document).ready(function(){
+  $(".nav").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 1500);
+  });
+});
+// CLOSE  Якорь. Плавный переход по клику на ссылку.
